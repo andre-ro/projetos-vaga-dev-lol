952783e3f81addd5d8d7348d33be6e81db58d3ca
André <67649422+andre-ro@users.noreply.github.com>
2025-08-27 19:32:52 -0300

    Add files via upload

---
+++ vitorbot twitter/bot.py
@@ -0,0 +1,35 @@
+import tweepy
+import datetime
+import time
+
+auth = tweepy.OAuthHandler("EpBQRgtusXhoCRPUFtdc2Tpij", "00DCVwlNeKsUKzSNqewfRbumLhzq3AsmLyOJltRIpxRiyW2KlN")
+auth.set_access_token("1338892474119557122-g7T5XVIoGoSy49rf1ZI95bj3oGzoAp", "R9MDnQI16QVABYPvdLImOm3JzSZEDmVg9uMW6Zg1SsUCE")
+api = tweepy.API(auth) 
+
+
+try:
+    api.verify_credentials()
+    print("Authentication Successful")
+except:
+    print("Authentication Error")
+
+while True:
+    x = datetime.datetime.now()
+    h = x.strftime("%H")
+    # m = x.strftime("%M")
+    if h >= '15':
+        f = open("./dias.txt", "r")           # read file
+        a = f.read()                        # a = str in txt
+        f = open("dias.txt", "w")           # write file
+        f.write(str(int(a)+1))              # file ++
+        f.close()
+
+        status = 'day '+a+' vitor kenzo fukuhara pelegatti bazinga' 
+        api.update_with_media('vitro.jpg', status)
+        
+        print('updated day '+a)
+        time.sleep(3600)
+    else:
+        print(h+'h waiting...')
+        time.sleep(3600)
+
